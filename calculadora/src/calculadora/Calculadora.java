b37729298e4bca44e360c93e030f897d870b3f7d
18863032-4 <18863032-4@SNO_LAB_A30415.inacap.cl>
2016-04-22 14:44:20 -0300

    romana

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 18863032-4
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=0;
+        String array[] = new String[11];
+        array[1]="I";
+        array[2]="II";
+        array[3]="III";
+        array[4]="IV";
+        array[5]="V";
+        array[6]="VI";
+        array[7]="VII";
+        array[8]="VIII";
+        array[9]="IX";
+        array[10]="X";        
+        String [] numeros ={"I","v","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C"};
+        Scanner dato=new Scanner(System.in);
+        System.out.println("introdusca su numero");  
+        String numero1 = dato.nextLine();
+        System.out.println("introduca su segundo numero");
+        String numero2=dato.nextLine();
+                 
+                
+            
+        
+                  
+                 
+                    
+                    
+                    
+                    
+                    
+                    
+    }
+}
+        
+    
+
+                    
+        
+       
+        
+        
+        
+      
+        
+        
+        
+    
+    
